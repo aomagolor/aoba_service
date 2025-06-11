feec3012e91512c6834827a832a3144d6fc47131
aomagolor <aomagolor@gmail.com>
2025-06-12 06:01:58 +0900

    sample2-1

--- new_acount.php
+++ new_acount.php
@@ -1,4 +1,21 @@
 <?php
-print_r($_POST);
-print_r($_FILES);
+    do {
+        $data = $_POST
+        if (!$data) {
+            // フォームの初期値
+            $data = [
+                'name' => '', 'mail' => '', 'password' => '', 'birth_month' => '', 'birth_day' => ''
+            ];
+            break;
+        }
+    } while(0);
+    
+    $name = trim(@$data['name']);
+    
+    if (!$name) {
+        $errors['name'] = '名前が入力されていません。';
+    }
+    else if (mb_strlen($name) > 10) {
+        $errors['name'] = '名前は１０文字以内で入力してください。';
+    }
 ?>
